3b462dd7b5841c7bb576f0181e298bbd5bd6e83b
root <anouarsouid@hotmail.fr>
2020-07-05 23:09:24 +0200

    ajout de swiftmailer pour l'envoi de mail

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Enqueue\MessengerAdapter\Bundle\EnqueueAdapterBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
